86d4e182c08b4c3b6d8aae470681ff9a043d3fa9
Rohit <rohitchaudharyrk.25@gmail.com>
2025-05-19 15:23:41 +0530

    commit

--- client/src/components/chatPage/ChatPage.jsx
+++ client/src/components/chatPage/ChatPage.jsx
@@ -10,7 +10,7 @@ import MessageList from "./MessageList";
 import ChatInput from "./ChatInput";
 
 // Server URL - replace with your actual backend URL
-const SERVER_URL = "http://localhost:8000";
+const SERVER_URL = import.meta.env.VITE_SERVER_URL;
 
 // Socket.io connection
 let socket;
